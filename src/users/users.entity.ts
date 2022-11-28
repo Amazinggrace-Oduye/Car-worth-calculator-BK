524b13fa3623c97c862c9f6e9c440924d853a760
Amazinggrace <grace.oduye@gmail.com>
2022-11-28 13:55:37 +0100

    add services and controllers

---
+++ src/users/users.entity.ts
@@ -0,0 +1,14 @@
+import { Column, PrimaryGeneratedColumn, Entity } from 'typeorm';
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column()
+  firstName: string;
+  @Column()
+  lastName: string;
+  @Column()
+  email: string;
+  @Column()
+  password: string;
+}
